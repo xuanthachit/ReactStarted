b93eb7dae99e45822cd4786f8fa5aa4a9843b832
xuanthachit <xuanthachit@gmail.com>
2018-01-15 17:25:42 +0700

    browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
 var liteServer = require('gulp-live-server');
+var browserSync = require('browser-sync');
 
-gulp.task('live-server', function(){
+gulp.task('lite-server', function(){
     var server = new liteServer('server/main.js');
     server.start();
+});
+
+gulp.task('serve', ['lite-server'], function(){
+    browserSync.init(null, {
+        proxy: 'http://localhost:7777',
+        port: 9001
+    })
 });
